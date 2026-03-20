7883602364e2a02de9fecaeb3fcca8f420e86bbd
JEMERSON DOS SANTOS <127410438+Jemerson07@users.noreply.github.com>
2026-03-20 15:46:22 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,32 @@
 export default function Navbar({ theme, setTheme }) {
   return (
     <nav className="navbar">
-      <h2>Jemerson Santos</h2>
+      <h2 style={{ fontWeight: 900 }}>Jemerson Santos</h2>
 
-      <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-        {theme === "dark" ? "☀️ Light" : "🌙 Dark"}
-      </button>
+      <div style={{ display: "flex", gap: "15px", alignItems: "center" }}>
+        {/* Botão LinkedIn */}
+        <a
+          href="https://www.linkedin.com/in/jemerson-santos"
+          target="_blank"
+          style={{
+            background: "#0077ff",
+            padding: "10px 14px",
+            borderRadius: "8px",
+            color: "white",
+            fontWeight: 700,
+            textDecoration: "none",
+          }}
+        >
+          Linkedin
+        </a>
+
+        {/* Switch de tema */}
+        <button
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        >
+          {theme === "dark" ? "☀️ Light" : "🌙 Dark"}
+        </button>
+      </div>
     </nav>
   );
 }
-
