ba380b3a6ddb81b8310af7bbf408fc475d54b504
JEMERSON DOS SANTOS <127410438+Jemerson07@users.noreply.github.com>
2026-03-20 15:05:03 -0300

    Create Services.jsx

---
+++ src/Services.jsx
@@ -0,0 +1,12 @@
+export default function Services() {
+  return (
+    <section>
+      <h2>Serviços</h2>
+
+      <div className="card">IA aplicada e automação inteligente</div>
+      <div className="card">Dashboards e insights baseados em dados</div>
+      <div className="card">Otimização de processos e governança</div>
+      <div className="card">Marketing tech e web design estratégico</div>
+    </section>
+  );
+}
