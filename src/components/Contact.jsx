4b72a0ad284fbcbdacd0c292c0720833095dea59
JEMERSON DOS SANTOS <127410438+Jemerson07@users.noreply.github.com>
2026-03-20 15:49:15 -0300

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,32 @@
+import { motion } from "framer-motion";
+
 export default function Contact() {
   return (
     <section>
       <h2>Contato</h2>
 
-      <div className="card">
+      <motion.div
+        className="glass contact-box"
+        initial={{ opacity: 0, y: 30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
         <p><strong>Email:</strong> jemerson456l@gmail.com</p>
-        <p><strong>LinkedIn:</strong> linkedin.com/in/jemersonsantos</p>
-        <p><strong>GitHub:</strong> jemerson07.github.io</p>
-      </div>
+
+        <p>
+          <strong>LinkedIn:</strong>
+          https://www.linkedin.com/in/jemerson-santos"
+            className="contact-btn"
+            target="_blank"
+          >
+            🔗 Acessar LinkedIn
+          </a>
+        </p>
+
+        <p>
+          <strong>WhatsApp:</strong>  
+        </p>
+      </motion.div>
     </section>
   );
 }
