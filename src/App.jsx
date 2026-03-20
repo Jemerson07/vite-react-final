0f454c5e0f4f7c18e09120b87a9e4d45e6beff5c
JEMERSON DOS SANTOS <127410438+Jemerson07@users.noreply.github.com>
2026-03-20 14:57:25 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import { useState } from "react";
+
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
-import Features from "./components/Features.jsx";
-export default function App(){
- const [theme,setTheme]=useState("dark");
- return (
-  <div className={theme}>
-    <Navbar theme={theme} setTheme={setTheme} />
-    <Hero />
-    <Features />
-  </div>
- );
+import About from "./components/About.jsx";
+import Skills from "./components/Skills.jsx";
+import Projects from "./components/Projects.jsx";
+import Services from "./components/Services.jsx";
+import Education from "./components/Education.jsx";
+import Numbers from "./components/Numbers.jsx";
+import Contact from "./components/Contact.jsx";
+import Footer from "./components/Footer.jsx";
+
+export default function App() {
+  const [theme, setTheme] = useState("dark");
+
+  return (
+    <div className={theme}>
+      <Navbar theme={theme} setTheme={setTheme} />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Services />
+      <Education />
+      <Numbers />
+      <Contact />
+      <Footer />
+    </div>
+  );
 }
