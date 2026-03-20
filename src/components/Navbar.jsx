0195640913223e982ebff7f362fc554bb46d3a4e
JEMERSON DOS SANTOS <127410438+Jemerson07@users.noreply.github.com>
2026-03-20 14:59:01 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
-export default function Navbar({theme,setTheme}){
- return (
-  <nav className="navbar">
-    <h2>Jemerson</h2>
-    <button onClick={()=>setTheme(theme==="dark"?"light":"dark")}>
-      Tema
-    </button>
-  </nav>
- );
+export default function Navbar({ theme, setTheme }) {
+  return (
+    <nav className="navbar">
+      <h2>Jemerson Santos</h2>
+
+      <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+        {theme === "dark" ? "☀️ Light" : "🌙 Dark"}
+      </button>
+    </nav>
+  );
 }
+
