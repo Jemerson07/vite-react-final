f6f5089a01965d3bc2d3b569e96c4bb0b4bd26bd
JEMERSON DOS SANTOS <127410438+Jemerson07@users.noreply.github.com>
2026-03-20 15:06:40 -0300

    Create Contact.jsx

---
+++ src/Contact.jsx
@@ -0,0 +1,13 @@
+export default function Contact() {
+  return (
+    <section>
+      <h2>Contato</h2>
+
+      <div className="card">
+        <p><strong>Email:</strong> jemerson456l@gmail.com</p>
+        <p><strong>LinkedIn:</strong> linkedin.com/in/jemersonsantos</p>
+        <p><strong>GitHub:</strong> jemerson07.github.io</p>
+      </div>
+    </section>
+  );
+}
