a1b2a2bae9b90fc44713cc598361e1cdd2a0cf3e
JEMERSON DOS SANTOS <127410438+Jemerson07@users.noreply.github.com>
2026-03-20 15:49:32 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer style={{ textAlign: "center", padding: 30, opacity: 0.4 }}>
-      © 2026 — Portfólio Profissional de Jemerson Santos
+    <footer className="footer">
+      © {new Date().getFullYear()} — Portfólio Profissional de Jemerson Santos
     </footer>
   );
 }
